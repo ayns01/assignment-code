01c5a1590d2b1668f5b7430e71effe024f1682a4
ayns <peenats1203@gmail.com>
2018-05-16 14:04:24 +0900

    Initial Commit

---
+++ Second/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Second
+//
+//  Created by 酒井綾菜 on 2018-05-16.
+//  Copyright © 2018 Ayana Sakai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
